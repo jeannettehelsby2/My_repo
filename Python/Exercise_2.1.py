3fdc0770936e269ca760e7e6b53149209c89b505
jeannettehelsby2 <jeannettevera@jeannettes-MBP.home>
2021-02-01 11:05:52 +0000

    new file

---
+++ Python/Exercise_2.1.py
@@ -0,0 +1,6 @@
+#length = 3
+#width = 4
+length = float(input("Please introduce the length"))
+width = float(input("Please introduce the width"))
+area = length * width  # we are using variables defined above
+print("The area of a rectangle room of length", length, "and width", width, "is", area)
